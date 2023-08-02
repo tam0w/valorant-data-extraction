09977a65309971843d8cb7d1ffdb0aedd210814e
Tamim <tammu1000@gmail.com>
2023-08-02 19:03:53 +0530

    one extra screenshot but working other than that

--- desktop-app/functions.py
+++ desktop-app/functions.py
@@ -1,10 +1,12 @@
 import time
-
+import numpy as np
 import pyautogui as py
+import cv2 as cv
 
 
 print(py.size())
-rounds = []
+ss = []
+
 time.sleep(3)
 
 print(py.position())
@@ -15,8 +17,30 @@ py.leftClick()
 py.moveTo(187,333,duration=0.45)
 py.leftClick()
 
-for i in range(23):
-    py.moveRel(63,0,duration=0.2)
-    py.leftClick()
-    if i == 11:
-        py.moveRel(-20, 0, duration=0.2)
+imageObj = py.screenshot()
+cv_imageObj = cv.cvtColor(np.array(imageObj), cv.COLOR_RGB2BGR)
+ss.append(cv_imageObj)
+def screenshots_rounds(total_rounds):
+
+    imageObj = py.screenshot()
+    cv_imageObj = cv.cvtColor(np.array(imageObj), cv.COLOR_RGB2BGR)
+    ss.append(cv_imageObj)
+
+    for i in range(total_rounds):
+
+        py.moveRel(63,0,duration=0.12)
+        py.leftClick()
+
+        imageObj = py.screenshot()
+        cv_imageObj = cv.cvtColor(np.array(imageObj), cv.COLOR_RGB2BGR)
+        ss.append(cv_imageObj)
+
+        if i == 11:
+            py.moveRel(-20, 0, duration=0.2)
+            continue
+
+screenshots_rounds(23)
+
+print(len(ss))
+
+
