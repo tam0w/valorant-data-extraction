d3e9ba3bccf1a1d4914f7485ed5489d7c13fe1d9
Tamim <tammu1000@gmail.com>
2023-08-02 18:15:56 +0530

    pyautogui working in valorant confirmed

--- desktop-app/functions.py
+++ desktop-app/functions.py
@@ -1,5 +1,22 @@
+import time
+
 import pyautogui as py
 
-def res():
-    x, y = py.size()
-    return x, y
+
+print(py.size())
+rounds = []
+time.sleep(3)
+
+print(py.position())
+
+py.moveTo(1020,190,duration=0.5)
+py.leftClick()
+
+py.moveTo(187,333,duration=0.45)
+py.leftClick()
+
+for i in range(23):
+    py.moveRel(63,0,duration=0.2)
+    py.leftClick()
+    if i == 11:
+        py.moveRel(-20, 0, duration=0.2)
