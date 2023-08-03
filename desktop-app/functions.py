dbd0c2e951e31c2a548fcc7ad4ed726f3913e196
Tamim <tammu1000@gmail.com>
2023-08-03 13:51:53 +0530

    cleaner functions

--- desktop-app/functions.py
+++ desktop-app/functions.py
@@ -3,48 +3,39 @@ import numpy as np
 import pyautogui as py
 import cv2 as cv
 
-
-print(py.size())
-ss = []
-
 time.sleep(3)
 
-print(py.position())
 
-py.moveTo(1020,190,duration=0.5)
-py.leftClick()
+def go_timeline():
+    py.leftClick(x=1020, y=190, duration=0.35)
+    py.leftClick(x=187, y=333, duration=0.3)
 
-py.moveTo(187,333,duration=0.45)
-py.leftClick()
 
 def screenshots_rounds(total_rounds):
+    tl_ss = []
 
-
-    ss = []
-
-    imageObj = py.screenshot()
-    cv_imageObj = cv.cvtColor(np.array(imageObj), cv.COLOR_RGB2BGR)
-    ss.append(cv_imageObj)
+    image = py.screenshot()
+    cv_image = cv.cvtColor(np.array(image), cv.COLOR_RGB2BGR)
+    tl_ss.append(cv_image)
 
     for i in range(total_rounds):
 
-        py.moveRel(63,0,duration=0.12)
+        py.moveRel(63, 0, duration=0.12)
         py.leftClick()
 
         if i == 11:
             py.moveRel(-20, 0, duration=0.2)
             continue
 
-        imageObj = py.screenshot()
-        cv_imageObj = cv.cvtColor(np.array(imageObj), cv.COLOR_RGB2BGR)
-        ss.append(cv_imageObj)
+        image = py.screenshot()
+        cv_image = cv.cvtColor(np.array(image), cv.COLOR_RGB2BGR)
+        tl_ss.append(cv_image)
+
+    return tl_ss
 
 
+go_timeline()
 
-    return ss
-
-scrim1 = screenshots_rounds(23)
+scrim1 = screenshots_rounds(19)
 
 print(len(scrim1))
-
-
